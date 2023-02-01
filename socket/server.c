4450861430ef1806a37563088fb7390732407d80
ChangSoo Kim <92871579+soochang0812@users.noreply.github.com>
2023-02-01 21:57:09 +0900

    Create server.c

---
+++ socket/server.c
@@ -0,0 +1,18 @@
+/**************************************************************************
+* Filename : server.c
+* Author : Chang Soo Kim <soochang0812@gmail.com>
+* description : Socket Server
+**************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main(int argc, char** argv[])
+{
+  return 0;
+}
